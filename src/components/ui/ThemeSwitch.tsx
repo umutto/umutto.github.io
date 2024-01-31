14adbf3ebca5d61c8900cf3c23474f7613c281d6
Umut Karakulak <umutkarakulak@hotmail.com>
2024-02-01 04:04:18 +0900

    feat: add theme switcher component

---
+++ src/components/ui/ThemeSwitch.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { themes } from "@/config";
+import { useEffect, useState } from "react";
+import { Icon } from "@/components";
+
+export default function ThemeSwitch() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? themes.light);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.querySelector("html")?.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const handleToggle = (e: any) => {
+    if (e.target.checked) {
+      setTheme(themes.light);
+    } else {
+      setTheme(themes.dark);
+    }
+  };
+
+  return (
+    <label className="swap swap-rotate">
+      <input
+        type="checkbox"
+        className="theme-controller"
+        onChange={handleToggle}
+        checked={theme === themes.light}
+      />
+      <Icon name="SunMedium" className="swap-on fill-current w-10 h-10" />
+      <Icon name="Moon" className="swap-off fill-current w-10 h-10" />
+    </label>
+  );
+}

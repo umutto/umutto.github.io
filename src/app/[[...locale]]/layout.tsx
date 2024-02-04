2105f2966333923e15643b8d9780e5ec0e3c8727
Umut Karakulak <umutkarakulak@hotmail.com>
2024-02-05 02:16:12 +0900

    feat: meta (title, desc) localization

--- src/app/[[...locale]]/layout.tsx
+++ src/app/[[...locale]]/layout.tsx
@@ -4,6 +4,8 @@ import AnimatedBackground from "@/components/AnimatedBackground";
 import { defaultLocale, localeKey, locales } from "@/config";
 
 import "../globals.css";
+
+import { getTranslator } from "@/utils/localization";
 import { jsonLdProfilePage } from "@/utils/structuredData";
 
 export async function generateStaticParams() {
@@ -13,59 +15,66 @@ export async function generateStaticParams() {
   return [...availableLocales, { locale: [""] }];
 }
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://umu.to"),
-  title: "Umut Karakulak's Portfolio: Web Development & Machine Learning",
-  description:
-    "Someone who is passionate about experimenting with innovative, state of the art technologies. Currently working as a full-stack developer with a touch of machine learning for good measure. Explore my portfolio and feel free to say hi!",
-  authors: {
-    name: "Umut Karakulak",
-  },
-  keywords: [
-    "Software Developer",
-    "Machine Learning",
-    "React",
-    "Nextjs",
-    "Web Frameworks",
-    "SQL Databases",
-    "GitHub",
-    "Full Stack Development",
-    "JavaScript",
-    "Python",
-    "SQL",
-    "Typescript",
-    "Open Source",
-    "Portfolio",
-    "Tokyo",
-    "Japan",
-  ],
-  openGraph: {
-    title: "Umut Karakulak's Portfolio: Web Development & Machine Learning",
-    description:
-      "Someone who is passionate about experimenting with innovative, state of the art technologies. Currently working as a full-stack developer with a touch of machine learning for good measure. Explore my portfolio and feel free to say hi!",
-    url: "https://umu.to",
-    siteName: "umu.to",
-    images: [
-      {
-        url: "https://umu.to/logo_3x2_lg.png", // Must be an absolute URL
-        width: 1800,
-        height: 1200,
-      },
-      {
-        url: "https://umu.to/logo_3x2.png", // Must be an absolute URL
-        width: 900,
-        height: 600,
-      },
-      {
-        url: "https://umu.to/logo_3x2_sm.png", // Must be an absolute URL
-        width: 600,
-        height: 400,
-      },
+export async function generateMetadata({
+  params,
+}: {
+  params?: { locale: localeKey };
+}): Promise<Metadata> {
+  const locale = params?.locale || defaultLocale;
+  const t = await getTranslator(locale, ["metadata"]);
+
+  return {
+    metadataBase: new URL("https://umu.to"),
+    title: t("Title"),
+    description: t("Description"),
+    authors: {
+      name: "Umut Karakulak",
+    },
+    keywords: [
+      "Software Developer",
+      "Machine Learning",
+      "React",
+      "Nextjs",
+      "Web Frameworks",
+      "SQL Databases",
+      "GitHub",
+      "Full Stack Development",
+      "JavaScript",
+      "Python",
+      "SQL",
+      "Typescript",
+      "Open Source",
+      "Portfolio",
+      "Tokyo",
+      "Japan",
     ],
-    locale: "en_US",
-    type: "website",
-  },
-};
+    openGraph: {
+      title: t("Title"),
+      description: t("Description"),
+      url: "https://umu.to",
+      siteName: "umu.to",
+      images: [
+        {
+          url: "https://umu.to/logo_3x2_lg.png", // Must be an absolute URL
+          width: 1800,
+          height: 1200,
+        },
+        {
+          url: "https://umu.to/logo_3x2.png", // Must be an absolute URL
+          width: 900,
+          height: 600,
+        },
+        {
+          url: "https://umu.to/logo_3x2_sm.png", // Must be an absolute URL
+          width: 600,
+          height: 400,
+        },
+      ],
+      locale: locale,
+      type: "website",
+    },
+  };
+}
 
 export default function LocaleLayout({
   children,
